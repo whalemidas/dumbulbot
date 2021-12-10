9fccf918eadaa4ab7dd5c194a8cb25688f529bcd
whalemidas <66910484+whalemidas@users.noreply.github.com>
2021-12-10 15:39:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import discord
-import os
 import random
 
+chamsae = 0
+marun = 0
+lebun = 0
+koful = 0
 
 client = discord.Client()
 
@@ -24,7 +27,31 @@ async def on_message(message):
         if command == "인사":
             await message.channel.send("그래")
         
-        
+        elif command == "반배정":
+            if len(message.author.roles) == 1:
+                i = random.randrange(1,5)
+                if i == 1:
+                    role = discord.utils.get(message.guild.roles, name="참새핀도르")
+                    await message.author.add_roles(role)
+                    await message.channel.send("흠...참새핀도르가 좋겠구나")
+                if i == 2:
+                    role = discord.utils.get(message.guild.roles, name = "마른데린")
+                    await message.author.add_roles(role)
+                    await message.channel.send("어디보자.. 마른데린~")
+                if i == 3:
+                    role = discord.utils.get(message.guild.roles, name = "코풀푸프")
+                    await message.author.add_roles(role)
+                    await message.channel.send("코풀푸프!")
+                if i == 4:
+                    role = discord.utils.get(message.guild.roles, name = "래번손톱")
+                    await message.author.add_roles(role)
+                    await message.channel.send("래번손톱으로 배정되었다.")
+            else:
+                await message.channel.send("이미 반배정이 완료되었다.")
+
+
+            
+
         
     if message.content.startswith("루모스"):
         await message.channel.send("빛이 당신의 어두운 미래를 밝게 비추었습니다.")
@@ -34,25 +61,6 @@ async def on_message(message):
     
     if message.content.startswith("아씨오"):
         await message.channel.send("뿌슝빠숑 휘유유우우웅(대충 뭔가 날아오는 소리)")
-        
-    if command == "반배정":
-            if message.author.get_roles is None:
-                i = random.randrange(1,5)
-                if i == 1:
-                    role = discord.utils.get(msg.guild.roles, name="참새핀도르")
-                    await message.author.add_roles(role)
-                    await message.channel.send("흠...참새핀도르가 좋겠구나")
-                if i == 2:
-                    role = discord.utils.get(message.guild.roles, name = "마른데린")
-                    await message.channel.send("어디보자.. 마른데린~")
-                if i == 3:
-                    role = discord.utils.get(message.guild.roles, name = "코풀푸프")
-                    await message.channel.send("코풀푸프!")
-                if i == 4:
-                    role = discord.utils.get(message.guild.roles, name = "래번손톱")
-                    await message.channel.send("래번손톱으로 배정되었다.")
-            else:
-                await message.channel.send("이미 반배정이 완료되었다.")
 
     
 
